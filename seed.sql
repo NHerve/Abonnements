fdda9e7cee44d5696c356ce0aaa72d63dcf9b02a
NHerve <herve.04@hotmail.fr>
2019-06-12 10:36:37 +0100

    ajout d'un satus d'abonnement

--- seed.sql
+++ seed.sql
@@ -25,6 +25,7 @@ CREATE TABLE Abonnement (
 	aboFKMag INT NOT NULL REFERENCES magazine(magId),
 	aboDateDebut DATE NOT NULL,
 	aboDateFin DATE,
+	aboStatus INT NOT NULL,
 	PRIMARY KEY(aboFKCli,aboFKMag)
 );
 
@@ -36,14 +37,14 @@ INSERT INTO magazine (magTitre, magNbVolumeAnnee,magPhotoCouverture,magDescripti
 INSERT INTO magazine (magTitre, magNbVolumeAnnee,magPhotoCouverture,magDescription,magPrixAnnuel) VALUES ('nord presse' , 365, 'https://cdni.rt.com/french/images/2016.10/article/5810e944c36188e3488b456f.jpg', 'Nordpresse est un site d''information belge. Commençant d''abord par s''inspirer de Sudpresse, le site utilise également des noms de domaines ressemblant à ceux de médias français pour crédibiliser ses informations.',99.98);
 INSERT INTO magazine (magTitre, magNbVolumeAnnee,magPhotoCouverture,magDescription,magPrixAnnuel) VALUES ('le monde' , 365, 'https://static1.ozap.com/articles/6/57/89/36/@/4617206-le-monde-article_default-2.jpg', 'Le Monde est un journal français fondé par Hubert Beuve-Méry en 1944. C''est l''un des derniers quotidiens français dits « du soir », qui paraît, daté du lendemain, à Paris en début d''après-midi ainsi que, un peu plus tard, dans certaines grandes villes, et est distribué en province le matin suivant.',99.98);
 
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (1,1,'2019-06-10','2020-06-10');
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (1,2,'2018-08-02','2019-08-02');
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (1,3,'2013-04-08','2014-04-08');
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (1,1,'2019-06-10','2020-06-10',1);
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (1,2,'2018-08-02','2019-08-02',1);
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (1,3,'2013-04-08','2014-04-08',0);
 
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (2,1,'2013-04-08','2014-04-08');
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (2,2,'2018-08-02','2019-08-02');
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (2,3,'2019-06-10','2020-06-10');
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (2,1,'2013-04-08','2014-04-08',0);
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (2,2,'2018-08-02','2019-08-02',1);
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (2,3,'2019-06-10','2020-06-10',1);
 
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (3,1,'2018-08-02','2019-08-02');
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (3,2,'2013-04-08','2014-04-08');
-INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin) VALUES (3,3,'2019-06-10','2020-06-10');
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (3,1,'2018-08-02','2019-08-02',1);
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (3,2,'2013-04-08','2014-04-08',0);
+INSERT INTO abonnement (aboFKCli,aboFKMag,aboDateDebut,aboDateFin,aboStatus) VALUES (3,3,'2019-06-10','2020-06-10',1);
